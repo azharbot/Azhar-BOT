35e328a786f970f80a1a83ed920e0dcf7d5fbd3c
azharbot <143462130+azharbot@users.noreply.github.com>
2023-10-17 12:15:02 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['923272399436', 'me', true],
+  ['923272399436', 'Azhar-Ali', true], 
   [''] 
 ] //Number of owners
 
